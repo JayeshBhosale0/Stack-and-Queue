03ec97a6bfe2c375cdc399d262a613aa1adfc9fc
Jayesh <jayesh.bhosale1812@gmail.com>
2021-11-11 18:46:22 +0530

    stack and queue

---
+++ Operation.java
@@ -0,0 +1,46 @@
+package practise;
+
+public class Operation {
+	public StackAndQueue addElement() {
+		StackAndQueue stackList = new StackAndQueue();
+		stackList.push(70);
+		stackList.push(30);
+		stackList.push(56);
+
+		stackList.display();
+		return stackList;
+	}
+
+	public void peek() {
+		StackAndQueue stackList = new StackAndQueue();
+		stackList.push(70);
+		stackList.push(30);
+		stackList.push(56);
+		stackList.peek();
+	}
+
+	public void pop() {
+		System.out.println("Before Pop:");
+		StackAndQueue stackList = addElement();
+		stackList.pop();
+		System.out.println("");
+		System.out.println("After Pop:");
+		stackList.display();
+	}
+
+	public void queueElement() {
+		StackAndQueue stackList = new StackAndQueue();
+		stackList.queue(56);
+		stackList.queue(30);
+		stackList.queue(70);
+		stackList.display();
+		System.out.println("");
+		stackList.peek();
+	}
+
+	public void dequeueElement() {
+		queueElement();
+		pop();
+	}
+
+}
